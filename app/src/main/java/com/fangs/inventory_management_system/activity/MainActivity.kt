122e91b0f19d22e9af634261f02e6c817e96ce12
Fangszx <jygrzn@gmail.com>
2021-08-04 21:58:37 +0800

    add recycler view for main activity

--- app/src/main/java/com/fangs/inventory_management_system/activity/MainActivity.kt
+++ app/src/main/java/com/fangs/inventory_management_system/activity/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.fangs.inventory_management_system.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.fangs.inventory_management_system.R
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
